fc0941ccc93f4de65e20491cb84a1256f787e4a2
Simon Schmid <mail@sschmid.com>
2016-09-18 21:18:53 +0200

    Added CodeGenerator project

---
+++ CodeGenerator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Reflection;
+
+namespace CodeGenerator {
+
+    class MainClass {
+
+        public static void Main(string[] args) {
+
+            if(args.Length != 1) {
+                printUsage();
+            } else {
+                generate(args[0]);
+            }
+
+            Console.Read();
+        }
+
+        static void generate(string path) {
+            try {
+
+                var assembly = Assembly.LoadFrom(path);
+
+                // Error
+                // Could not load file or assembly '.../Assembly-CSharp.dll' or one of its dependencies
+
+            } catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        static void printUsage() {
+            Console.WriteLine("Usage:");
+        }
+    }
+}
---
+++ CodeGenerator/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyTitle("CodeGenerator")]
+[assembly: AssemblyDescription("Entitas Code Generator")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Entitas Code Generator")]
+[assembly: AssemblyCopyright("sschmid")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0")]
